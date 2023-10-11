1e8944576440af22c340f6bf4a8538c7dae3546a
a.skorik <a.skorik@petrovich.ru>
2023-10-11 20:33:29 +0300

    edited test_shop.py

--- homework/test_shop.py
+++ homework/test_shop.py
@@ -2,13 +2,18 @@
 Протестируйте классы из модуля homework/models.py
 """
 import pytest
-
-from homework.models import Product
+from random import randint
+from homework.models import Product, Cart
 
 
 @pytest.fixture
 def product():
     return Product("book", 100, "This is a book", 1000)
+
+
+@pytest.fixture()
+def cart():
+    return Cart()
 
 
 class TestProducts:
@@ -17,7 +22,7 @@ class TestProducts:
     Например, текущий класс группирует тесты на класс Product
     """
 
-    @pytest.mark.parametrize("quantity", [999, 1000])
+    @pytest.mark.parametrize("quantity", [0, 1, 999, 1000])
     def test_product_check_quantity(self, product, quantity):
         # TODO напишите проверки на метод check_quantity
         assert product.check_quantity(quantity)
@@ -45,3 +50,34 @@ class TestCart:
         На некоторые методы у вас может быть несколько тестов.
         Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
     """
+
+    @pytest.mark.parametrize("actual_quantity, expected_quantity", [(0, 0), (1, 1), (10, 10)])
+    def test_products_add_to_cart(self, cart, product, actual_quantity, expected_quantity):
+        cart.add_product(product, actual_quantity)
+        assert cart.products[product] == expected_quantity
+        add_count = randint(0, 100)
+        cart.add_product(product, add_count)
+        assert cart.products[product] == expected_quantity + add_count
+
+    @pytest.mark.parametrize("add_products, rm_products, expected_count", [(11, 1, 10), (10, 1, 9)])
+    def test_products_del_from_cart(self, cart, product, add_products, rm_products, expected_count):
+        cart.add_product(product, add_products)
+        cart.remove_product(product, rm_products)
+        assert cart.products[product] == expected_count
+
+    @pytest.mark.parametrize("add_products, rm_products", [(0, 0), (1, 1), (0, 1)])
+    def test_products_del_from_cart_all(self, cart, product, add_products, rm_products):
+        cart.add_product(product, add_products)
+        cart.remove_product(product, rm_products)
+        assert product not in cart.products.keys()
+
+    @pytest.mark.parametrize("quantity", [0, 1, 999, 1000])
+    def test_cart_clear(self, cart, product, quantity):
+        cart.add_product(product, quantity)
+        cart.clear()
+        assert product not in cart.products.keys()
+
+    @pytest.mark.parametrize("quantity", [0, 1, 999, 1000])
+    def test_cart_get_total_price(self, cart, product, quantity):
+        cart.add_product(product, quantity)
+        assert cart.get_total_price() == product.price * quantity
